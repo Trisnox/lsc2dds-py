726aa5d18545204630027ce796846e3a43ef0b4c
Trisnox <66292369+Trisnox@users.noreply.github.com>
2023-05-21 01:43:31 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,41 @@
+import os
+
+def convert(array):
+    result = []
+    for byte in array:
+        temp = (byte & 0x7) << 0x5
+        byte >>= 0x3
+        byte |= temp
+        byte ^= 0xff
+        result.append(byte)
+    return result
+
+f = input("image/folder location: ").strip('"')
+if os.path.isfile(f):
+    file_path = f.replace('.lsc', '.dds')
+
+    with open(f, 'rb') as file:
+        data = file.read()
+        array_byte = list(data)
+
+    with open(file_path, 'wb') as dds:
+        dds.write(bytearray(convert(array_byte)))
+        print("Written file:", file_path)
+else:
+    for x, _, y in os.walk(f):
+        for z in y:
+            if not z.endswith('.lsc'):
+                continue
+            
+            file = x + '\\' + z
+            file_path = x + '\\' + z.replace('.lsc', '.dds')
+
+            with open(file, 'rb') as raw_file:
+                data = raw_file.read()
+                array_byte = list(data)
+
+            with open(file_path, 'wb') as dds:
+                dds.write(bytearray((convert(array_byte))))
+                print("Saved image: ", file_path)
+
+input('Press enter to exit')
